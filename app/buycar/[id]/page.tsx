9773f6777128246635edfab337a0eb3f2fa036ff
PawarisSueaaeim <pawaris.sueaaeim@gmail.com>
2023-09-22 09:10:47 +0700

    feat(RG-0001): add detail pages

--- app/buycar/[id]/page.tsx
+++ app/buycar/[id]/page.tsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import { Box, Typography } from "@mui/material";
+import axios from "axios";
+import Image from "next/image";
+import React, { Fragment } from "react";
 
 type Props = {
-    params: {id: string}
-}
+  params: { id: string };
+};
 
-export default function Detail({params}: Props) {
+export default async function Detail({ params }: Props) {
+  const getAllPokemon = process.env.NEXT_PUBLIC_API_URL;
+
+  const response = await fetch(`${getAllPokemon}/${params.id}`, {
+    next: { revalidate: 10 },
+  });
+  const data = await response.json();
+
   return (
-    <div>Detail: car_ID = {params.id}</div>
-  )
+    <Box
+      display={"flex"}
+      flexDirection={"column"}
+      alignItems={"center"}
+      marginX={8}
+    >
+      <Box marginTop={4}>
+        <Image
+          src={data?.sprites?.other?.home?.front_default}
+          width={250}
+          height={250}
+          alt={`${data.name}`}
+        />
+      </Box>
+      <Typography variant="h2" style={{ color: "#0E2C77" }}>
+        {data.name}
+      </Typography>
+      <Typography variant="h5" style={{ color: "#0E2C77" }}>
+        {data.name}
+      </Typography>
+    </Box>
+  );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,31 +1,58 @@
-"use client"
-import React, { useEffect, useState } from "react";
+"use client";
+import React, { Fragment, useEffect, useState } from "react";
 import classes from "@/style/components/navbar.module.css";
 import Box from "@mui/material/Box";
 import Image from "next/image";
 import Link from "next/link";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const isMobileMode = useMediaQuery("(max-width:600px)");
+
+  useEffect(() => {
+    setShowMenu(isMobileMode);
+  }, [isMobileMode]);
+
   return (
     <Box className={classes.container}>
-      <div className={classes.logo}>
+      <Box className={classes.logo}>
         <Link href="/">
-          <Image src="/image/rapid-logo.png" alt="logo" width={150} height={50} />
+          <Image
+            src="/image/rapid-logo.png"
+            alt="logo"
+            width={150}
+            height={50}
+          />
         </Link>
-      </div>
-      <div className={classes.menu}>
-        <ul>
-          <li>
-            <Link href="/">หน้าแรก</Link>
-          </li>
-          <li>
-            <Link href="/buycar">ซื้อรถ</Link>
-          </li>
-          <li>
-            <Link href="/salecar">ขายรถ</Link>
-          </li>
-        </ul>
-      </div>
+      </Box>
+      <Box className={classes.menu}>
+        {showMenu ? (
+          <Box marginX={6}>
+            <MenuIcon onClick={() => setShowMenu(!showMenu)} />
+          </Box>
+        ) : (
+          <Fragment>
+            {isMobileMode && (
+              <CloseIcon onClick={() => setShowMenu(!showMenu)} />
+            )}
+            <ul>
+              <li>
+                <Link href="/">หน้าแรก</Link>
+              </li>
+              <li>
+                <Link href="/buycar">ซื้อรถ</Link>
+              </li>
+              <li>
+                <Link href="/salecar">ขายรถ</Link>
+              </li>
+            </ul>
+          </Fragment>
+        )}
+      </Box>
     </Box>
   );
 };
