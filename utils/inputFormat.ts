039723d0e55b81873886854672f938cb52480fe8
PawarisSueaaeim <97650022+PawarisSueaaeim@users.noreply.github.com>
2023-10-10 21:11:50 +0700

    refactor: verify form (#26)

--- utils/inputFormat.ts
+++ utils/inputFormat.ts
@@ -1,14 +1,14 @@
 export const isThaiText = (inputText: string): boolean => {
-    const regexPattern = /^[ก-ฮะ-ไ่-๋์]*$/;
+    const regexPattern = /^[ก-ฮะ-ไ่-๋์\s]*$/;
     return regexPattern.test(inputText);
 };
 
 export const isPhoneNumber = (inputText: string): boolean => {
-    const regexPattern = /^[0-9]*$/;
+    const regexPattern = /^[0-9]{0,10}$/;
     return regexPattern.test(inputText);
 }
 
 export const isEmail = (inputText: string): boolean => {
-    const regexPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    const regexPattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
     return regexPattern.test(inputText);
 }
