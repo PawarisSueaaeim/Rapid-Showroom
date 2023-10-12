25e896cc0ed9603d37dcd771019e0792223d4091
PawarisSueaaeim <97650022+PawarisSueaaeim@users.noreply.github.com>
2023-10-12 17:18:07 +0700

    feature: integrate api showroom v1 (#28)

--- components/common/form/selectionCustom.tsx
+++ components/common/form/selectionCustom.tsx
@@ -1,4 +1,5 @@
-import * as React from 'react';
+'use client'
+import React, {useEffect, useState} from 'react';
 import Box from '@mui/material/Box';
 import classes from '@/style/components/common/form.module.css';
 
@@ -6,12 +7,23 @@ type Props = {
     id: string,
     type?: string,
     data: {}[],
+    value?: string | number,
+    onOptionChange?: (event: any) => void
 };
 
-export default function SelectionCustom({id,data,type} : Props) {
+export default function SelectionCustom({id,data,type,value,onOptionChange} : Props) {
+  const selectedOption = value;
+
+  const handleSelected = (event: any) => {
+    const newOption = event.target.value;
+    //@ts-ignore
+    onOptionChange(newOption);
+  };
+
+
   return (
     <Box id={id} display={"flex"} justifyContent={"center"} alignItems={"center"}>
-        <select className={type === "unborder" ? classes.select_custom : classes.select_blue}>
+        <select value={selectedOption} onChange={handleSelected} className={type === "unborder" ? classes.select_custom : classes.select_blue}>
           {data.map((item:any, index:number) => {
             return (
               <option key={index} value={item.value}>{item.label}</option>
