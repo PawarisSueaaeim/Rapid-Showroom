058a4fb5a612b06eee2654a89c7b353f0872a04d
PawarisSueaaeim <97650022+PawarisSueaaeim@users.noreply.github.com>
2023-10-13 17:48:46 +0700

    refactor: vehicle detail page carousel style (#30)

--- components/modules/index.ts
+++ components/modules/index.ts
@@ -4,5 +4,4 @@ export {default as SalecarWithRapid } from './salecarWithRapid';
 export {default as ContactSection } from './contactSection';
 export {default as ShowcarSection } from './showcarSection';
 export {default as DealerMeet } from './dealerMeet';
-export {default as Carousel } from './carousel';
 export {default as SearchFilter } from './searchFilter';
