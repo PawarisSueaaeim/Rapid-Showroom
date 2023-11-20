bc01206b3e3b7c99839c786afcda34c5b69152c8
PawarisSueaaeim <pawaris.sueaaeim@gmail.com>
2023-11-20 21:28:38 +0700

    fix: env

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    env: {
+        NEXT_PUBLIC_GOOGLE_RECAPTCHA: "6LfRI1UoAAAAAOKL7AWcsLDmioNDKeUuzdCFjtcX"
+    },
     images: {
         domains: ['laravel.rapidmotors.tech'],
     },
