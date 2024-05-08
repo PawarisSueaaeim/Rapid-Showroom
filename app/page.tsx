99be552684797a5034fd558a060aba6990e5ce79
PawarisSueaaeim <pawaris.sueaaeim@gmail.com>
2024-05-08 21:31:05 +0700

    refactor: home page and css

--- app/page.tsx
+++ app/page.tsx
@@ -29,88 +29,86 @@ export default function Home({}: Props) {
         </Box>
       </Box>
       <Box className={classes.bg_image_02}>
-        <Box className={classes.backdropWhite}>
-          <Box className={classes.title_2_page}>
-            <span className="fs-24px fw-500">Rapid Certified Car</span>
-            <span className="fs-16px">
-              รับประกันคุณภาพ โดยผู้เชี่ยวชาญตรวจเช็คกว่า 50 รายการ
-              ซื้อขายรถกับเรา ราคาดี ถูกใจ ไร้กังวล
-            </span>
-            <Grid container>
-              <Grid item xs={12} md={6}>
-                <Box className={classes.why_rapid}>
-                  <span className="fs-22px">
-                    ซื้อรถกับ Rapid Auto นัดพบจบเลย
-                  </span>
-                  <Box className={classes.why_rapid_list}>
-                    <Box className={classes.icon_with_text}>
-                      <Image
-                        src="/icons/icon-wing.png"
-                        alt="icon-wing"
-                        width={50}
-                        height={50}
-                      />
-                      <span className="fs-16px">สภาพนางฟ้า</span>
-                    </Box>
-                    <Box className={classes.icon_with_text}>
-                      <Image
-                        src="/icons/icon-thumbs-up.png"
-                        alt="icon-thump-up"
-                        width={50}
-                        height={50}
-                      />
-                      <span className="fs-16px">งานดี ไม่มีย้อมแมว</span>
-                    </Box>
-                    <Box className={classes.icon_with_text}>
-                      <Image
-                        src="/icons/icon-cash-flow.png"
-                        alt="icon-cash-flow"
-                        width={50}
-                        height={50}
-                      />
-                      <span className="fs-16px">จัด โอน จบแน่นอน</span>
-                    </Box>
+        <Box className={classes.bg_container_2}>
+          <div className="fs-24px fw-500">Rapid Certified Car</div>
+          <span className="fs-16px">
+            รับประกันคุณภาพ โดยผู้เชี่ยวชาญตรวจเช็คกว่า 50 รายการ
+            ซื้อขายรถกับเรา ราคาดี ถูกใจ ไร้กังวล
+          </span>
+          <Grid container>
+            <Grid item xs={12} md={6}>
+              <Box className={classes.why_rapid}>
+                <span className="fs-22px">ซื้อรถกับ Rapid Auto นัดพบจบเลย</span>
+                <Box className={classes.why_rapid_list}>
+                  <Box className={classes.icon_with_text}>
+                    <Image
+                      src="/icons/icon-wing.png"
+                      alt="icon-wing"
+                      width={50}
+                      height={50}
+                    />
+                    <span className="fs-16px">สภาพนางฟ้า</span>
+                  </Box>
+                  <Box className={classes.icon_with_text}>
+                    <Image
+                      src="/icons/icon-thumbs-up.png"
+                      alt="icon-thump-up"
+                      width={50}
+                      height={50}
+                    />
+                    <span className="fs-16px">งานดี ไม่มีย้อมแมว</span>
+                  </Box>
+                  <Box className={classes.icon_with_text}>
+                    <Image
+                      src="/icons/icon-cash-flow.png"
+                      alt="icon-cash-flow"
+                      width={50}
+                      height={50}
+                    />
+                    <span className="fs-16px">จัด โอน จบแน่นอน</span>
                   </Box>
                 </Box>
-              </Grid>
-              <Grid item xs={12} md={6}>
-                <Box className={classes.sell_with_rapid}>
-                  <span className="fs-22px">
-                    ขายรถกับ Rapid Auto จบใน 45 นาที
-                  </span>
-                  <Box className={classes.why_rapid_list}>
-                    <Box className={classes.icon_with_text}>
-                      <Image
-                        src="/icons/icon-best-price.png"
-                        alt="icon-best-price"
-                        width={50}
-                        height={50}
-                      />
-                      <span className="fs-16px">ราคาโคตรดี</span>
-                    </Box>
-                    <Box className={classes.icon_with_text}>
-                      <Image
-                        src="/icons/icon-money.png"
-                        alt="icon-money"
-                        width={50}
-                        height={50}
-                      />
-                      <span className="fs-16px">รับเงินทันที</span>
-                    </Box>
-                    <Box className={classes.icon_with_text}>
-                      <Image
-                        src="/icons/icon-checkhand.png"
-                        alt="icon-checkhand"
-                        width={50}
-                        height={50}
-                      />
-                      <span className="fs-16px">ที่นี่ที่เดียว</span>
-                    </Box>
-                  </Box>
-                </Box>
-              </Grid>
+              </Box>
             </Grid>
-          </Box>
+            <Grid item xs={12} md={6}>
+              <Box className={classes.sell_with_rapid}>
+                <span className="fs-22px">
+                  ขายรถกับ Rapid Auto จบใน 45 นาที
+                </span>
+                <Box className={classes.why_rapid_list}>
+                  <Box className={classes.icon_with_text}>
+                    <Image
+                      src="/icons/icon-best-price.png"
+                      alt="icon-best-price"
+                      width={50}
+                      height={50}
+                    />
+                    <span className="fs-16px">ราคาโคตรดี</span>
+                  </Box>
+                  <Box className={classes.icon_with_text}>
+                    <Image
+                      src="/icons/icon-money.png"
+                      alt="icon-money"
+                      width={50}
+                      height={50}
+                    />
+                    <span className="fs-16px">รับเงินทันที</span>
+                  </Box>
+                  <Box className={classes.icon_with_text}>
+                    <Image
+                      src="/icons/icon-checkhand.png"
+                      alt="icon-checkhand"
+                      width={50}
+                      height={50}
+                    />
+                    <span className="fs-16px">ที่นี่ที่เดียว</span>
+                  </Box>
+                </Box>
+              </Box>
+            </Grid>
+          </Grid>
+        </Box>
+        <Box className={classes.btn_container}>
           <Box className={classes.btn_group}>
             <ButtonGroup />
           </Box>
